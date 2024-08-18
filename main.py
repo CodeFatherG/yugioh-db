03dfb6bc2a5b4ef4d144adbbcb95534c6413e452
ThePapaG <gdare1@yahoo.com.au>
2024-08-18 15:05:35 +1000

    Update simple data

--- main.py
+++ main.py
@@ -58,8 +58,14 @@ def download_image(image_url, image_path):
     if os.path.exists(image_path):
         return
 
-    print("Downloading " + image_url)
+    print("\tDownloading " + image_url + " to " + image_path)
     response = requests.get(image_url)
+
+    if not response.ok or response.status_code != 200:
+        print("Failed to download image " + image_url)
+        print(response.text)
+        return
+
     with open(image_path, "wb") as f:
         f.write(response.content)
 
@@ -74,7 +80,7 @@ def download_images(card):
 def save_card_info(card):
     info_path = os.path.join(get_card_path(card['id']), "info.json")
 
-    print("Saving card info for " + card['name'])
+    print("\tSaving card info for " + card['name'])
     with open(info_path, "w") as w:
         json.dump(card, w)
 
@@ -95,9 +101,38 @@ def compare_hash(card) -> bool:
         old_hash = r.read()
     return card_hash == old_hash
 
+def sync_card(card):
+    save_card_info(card)
+    download_images(card)
+    store_hash(card)
+
+def update_meta_json(start_time, end_time, cards_processed, total_cards, cards_added):
+    meta_file = "meta.json"
+    entry = {
+        "triggered_time": start_time.isoformat(),
+        "completed_time": end_time.isoformat(),
+        "time_taken": str(end_time - start_time),
+        "cards_processed": cards_processed,
+        "cards_found": total_cards,
+        "cards_added": cards_added
+    }
+    
+    if os.path.exists(meta_file):
+        with open(meta_file, "r") as f:
+            data = json.load(f)
+    else:
+        data = []
+    
+    data.append(entry)
+    
+    with open(meta_file, "w") as f:
+        json.dump(data, f, indent=2)
+
 def main():
     parser = argparse.ArgumentParser(description='Download Yu-Gi-Oh! card data')
     parser.add_argument('--card-count', type=int, help='Number of cards to download (optional)')
+    parser.add_argument('--card-id',    type=int, help='The card id to sync (optional)')
+    parser.add_argument('--card-name',  type=int, help='The card name to sync (optional)')
     args = parser.parse_args()
 
     response = requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
@@ -110,27 +145,35 @@ def main():
 
     print(f"Processing {cards_to_process} cards...")
 
-    with open("meta.txt", "w") as w:
-        w.write(datetime.now().isoformat() + ":\t" + str(cards_to_process) + " cards" + "\n")
-
+    start_time = datetime.now()
+    
     cards_processed = 0
+    cards_added = 0
     for card in cardinfo_json["data"]:
         try:
-            card = process_card(card)
-            if compare_hash(card):
-                continue
-
             print("Processing card " + card["name"] + " (" + str(cards_processed + 1) + "/" + str(cards_to_process) + ")")
+            card = process_card(card)
 
-            save_card_info(card)
-            download_images(card)
-            store_hash(card)
+            if not compare_hash(card):
+                sync_card(card)
+
+                cards_added += 1
+                if (cards_added >= cards_to_process):
+                    break
 
             cards_processed += 1
-            if (cards_processed >= cards_to_process):
-                break
+
         except Exception as e:
             print("Error processing card " + card["name"] + ":", e)
+
+    end_time = datetime.now()
+
+    update_meta_json(start_time, 
+                     end_time, 
+                     cards_processed, 
+                     cards_to_process,
+                     cards_added)
+
 
 if __name__ == "__main__":
     main()
